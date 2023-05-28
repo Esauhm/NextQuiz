35f49ab7758d989cd59e8a443134c12a6e789d6d
Juan92A <acunajuan429@gmail.com>
2023-05-28 15:27:07 -0600

    bug 1.0

--- app/src/main/java/sv/edu/catolica/nextquiz/PreguntaActivity.java
+++ app/src/main/java/sv/edu/catolica/nextquiz/PreguntaActivity.java
@@ -9,7 +9,6 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.animation.DecelerateInterpolator;
