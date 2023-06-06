a4108eb6374e7a8172723466cc567a84b4bf6184
Juan92A <acunajuan429@gmail.com>
2023-06-06 13:39:50 -0600

    cambio de modo

--- app/src/main/java/sv/edu/catolica/nextquiz/MainActivity.java
+++ app/src/main/java/sv/edu/catolica/nextquiz/MainActivity.java
@@ -1,18 +1,22 @@
 package sv.edu.catolica.nextquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,11 +26,16 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout home, nosotros, nuevos, logout, perfil;
     Button boton1, boton2, boton3, boton4;
 
+    Switch switcher;
+    boolean nightMODE;
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         drawerLayout = findViewById(R.id.drawerlayout);
         menu = findViewById(R.id.menu);
@@ -35,6 +44,31 @@ public class MainActivity extends AppCompatActivity {
         nuevos = findViewById(R.id.nuevos);
         logout = findViewById(R.id.logOut);
         perfil = findViewById(R.id.perfil);
+        switcher = findViewById(R.id.switcher);
+
+        sharedPreferences= getSharedPreferences("MODE", Context.MODE_PRIVATE);
+        nightMODE=sharedPreferences.getBoolean("night",false);
+
+        if (nightMODE){
+            switcher.setChecked(true);
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        }
+
+        switcher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (nightMODE){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    editor= sharedPreferences.edit();
+                    editor.putBoolean("night",false);
+                }else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    editor= sharedPreferences.edit();
+                    editor.putBoolean("night",true);
+                }
+                editor.apply();
+            }
+        });
 
         menu.setOnClickListener(new View.OnClickListener() {
             @Override
